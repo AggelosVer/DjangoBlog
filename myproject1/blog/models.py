19feef6637d58d26aafd54c22513c93cabb7509a
AggelosVer <verykiosaggelos@gmail.com>
2025-04-01 21:17:59 +0300

    complete tasks
    superuser
    username/password: user

---
+++ myproject1/blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from  django.conf import settings
+
+class Status(models.TextChoices):
+    DRAFT= "DF", "Draft"
+    PUBLISHED = "P", "Published"
+
+# Create your models here.
+class Post(models.Model):
+ author= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+ tittle = models.CharField(max_length=255)
+ slug=models.SlugField()
+ body = models.TextField()
+ publish = models.DateTimeField()
+ created = models.DateTimeField(auto_now_add=True)
+ updated = models.DateTimeField(auto_now=True)
+ status = models.CharField(max_length=2,choices=Status.choices, default=Status.DRAFT)
+ 
