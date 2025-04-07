ece519ae7c415af14f9ac33be8f9280f1f1f955e
AggelosVer <verykiosaggelos@gmail.com>
2025-04-07 21:31:49 +0300

    παρτ 3 1-2-3

--- myproject1/blog/models.py
+++ myproject1/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from  django.conf import settings
+from django.urls import reverse
 
 class Status(models.TextChoices):
     DRAFT= "DF", "Draft"
@@ -15,4 +16,12 @@ class Post(models.Model):
  created = models.DateTimeField(auto_now_add=True)
  updated = models.DateTimeField(auto_now=True)
  status = models.CharField(max_length=2,choices=Status.choices, default=Status.DRAFT)
+
  
+def get_absolute_url(self):
+    return reverse('blog:post_detail', args=[
+        self.publish.year,
+        self.publish.month,
+        self.publish.day,
+        self.slug
+    ])
--- myproject1/blog/views.py
+++ myproject1/blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
@@ -6,9 +7,19 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(status='PUBLISHED')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    posts_list = Post.objects.all().order_by('-publish')
+    #selidopoihsh
+    paginator = Paginator(posts_list, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/post/list.html', {'page_obj': page_obj})
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status='PUBLISHED')  
-    return render(request, 'blog/post_detail.html', {'post': post})
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
+    return render(request, 'blog/post/detail.html', {'post': post})
