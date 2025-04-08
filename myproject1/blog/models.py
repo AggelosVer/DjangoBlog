3705661cf1d3e2034ef30331f7849522a0af768f
AggelosVer <verykiosaggelos@gmail.com>
2025-04-09 01:50:32 +0300

    title clickable

--- myproject1/blog/models.py
+++ myproject1/blog/models.py
@@ -10,7 +10,7 @@ class Status(models.TextChoices):
 class Post(models.Model):
  author= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
  title = models.CharField(max_length=255)
- slug=models.SlugField()
+ slug= models.SlugField()
  body = models.TextField()
  publish = models.DateTimeField()
  created = models.DateTimeField(auto_now_add=True)
@@ -18,8 +18,8 @@ class Post(models.Model):
  status = models.CharField(max_length=2,choices=Status.choices, default=Status.DRAFT)
 
  
-def get_absolute_url(self):
-    return reverse('blog:post_detail', args=[
+ def get_absolute_url(self):
+     return reverse('blog:post_detail', args=[
         self.publish.year,
         self.publish.month,
         self.publish.day,
